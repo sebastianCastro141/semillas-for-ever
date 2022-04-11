2035a4101e221b0990fb69cfa3a6f3ed04adc8cb
sebastianCastro141 <sebastiancastro141@gmail.com>
2022-04-11 20:10:37 -0300

    implementacion a base de test

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,37 +1,31 @@
 package ar.edu.unahur.obj2.semillas
 
-class Menta(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
-        TODO("Te la debo, amigue...")
-    }
-
-    fun daSemillas(): Boolean {
-        TODO("Colgue, che...")
-    }
-
-    fun esFuerte(): Boolean {
-        TODO("Estas seguro que lo queres implementar así?")
-    }
-
+abstract class Planta(var altura: Double, val anioSemilla: Int) {
+    open fun horasDeSolQueTolera() = 7
+    fun esFuerte() = horasDeSolQueTolera()>9
+    open fun daSemillas() = this.esFuerte()
+    abstract fun espacio(): Double
 }
 
-class Soja(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
-        TODO("Lee un poco más el enunciado...")
-    }
-
-    fun daSemillas(): Boolean {
-        TODO("Dalee")
-    }
-
-    fun esFuerte(): Boolean {
-        TODO("Así me implementas mejor")
-    }
+class Menta(altura: Double,  anioSemilla: Int) : Planta(altura, anioSemilla)
+{
+    override fun daSemillas() =  super.daSemillas() || altura > 0.4
+    override fun espacio() = altura + 1.0
 }
 
-class Planta(var altura: Double, val anioSemilla: Int) {
-    fun espacio() {
-        TODO("Pasaron cosas")
+class Soja( altura: Double, anioSemilla: Int) : Planta(altura, anioSemilla)
+{
+    override fun horasDeSolQueTolera(): Int  {
+        return  when {
+            altura < 0.5  -> 6
+            altura < 1    -> 8
+            else          -> 12
+        }
     }
-
+    override fun espacio() = altura/2
+    override fun daSemillas() = super.esFuerte() or (this.semillaPosteriorA2007() && (altura>0.75 && altura<0.9))
+    fun semillaPosteriorA2007() = this.anioSemilla > 2007
 }
+
+
+

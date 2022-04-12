ee5c8a4510fc3638f27a2aedd3d627290eb9ffff
sebastianCastro141 <sebastiancastro141@gmail.com>
2022-04-12 16:55:50 -0300

    Estapa 3 completa

--- src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
@@ -2,19 +2,16 @@ package ar.edu.unahur.obj2.semillas
 
 
 
+import io.kotest.assertions.throwables.shouldThrowMessage
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 
 class SemillasTest : DescribeSpec ({
-    // hay una clase Planta que tiene por atributos
-    // anioSemilla, altura
+
     describe("Creación de las plantas") {
-        /*si tenemos una quinoa que ocupa 0.2 m2 y su semilla de origen es
-        de 2010, se trata de una planta que da semillas.
-        si tenemos una planta que ocupa 0.9 m2 pero cuya semilla de origen es de 2006,
-        también da semillas.*/
+
         val menta = Menta(1.0, 2021)
         val mentita = Menta(0.3, 2021)
         val soja = Soja(0.6, 2009)
@@ -61,11 +58,7 @@ class SemillasTest : DescribeSpec ({
             Math.ceil(superficie).shouldBe(4)
         }
         describe("Parcela"){
-            /*Una parcela de 20 metros de ancho por 1 metro de largo que recibe 10 horas de sol por día,
-            tiene una superficie de 20 metros cuadrados y la cantidad máxima de plantas que tolera es 4.
-            Si a esa parcela le plantamos 4 plantas de soja de más de 1 metro (que toleran 12 horas de sol),
-            no tendría complicaciones. Si intentaramos agregar una quinta planta, se superaría la cantidad máxima y
-            nos arrojaría un error.*/
+
             val parcela = Parcela(20.0,1.0,10, mutableListOf<Planta>())
             val sojaParaParcela = Soja(1.2,2011)
             val plantaQueSuperaLaCantidadMax = Menta(0.3,2012)
@@ -86,6 +79,9 @@ class SemillasTest : DescribeSpec ({
             it("complicaciones")
             {
                 parcela.tieneComplicaciones().shouldBeFalse()
+            }
+            it("supera la cantidad maxima"){
+                shouldThrowMessage("Ya no hay lugar en esta parcela"){parcela.plantarUnaPlanta(plantaQueSuperaLaCantidadMax)}
             }
 
 
